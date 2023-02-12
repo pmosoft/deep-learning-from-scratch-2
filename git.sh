1f3697a971683c8137f5a64a1077312f44ed6822
pmosoft <pmosoft@gmail.com>
2023-02-12 12:19:38 +0900

    --

---
+++ git.sh
@@ -0,0 +1,12 @@
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/pmosoft/deep-learning-from-scratch-2.git
+git push -u origin main
+
+git add *.*
+git commit -m "--"
+
+
+git push origin +main
